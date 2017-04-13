9f30880eae58eb50cc1ac775321bd037b7f9a370
Andriy Dreychan <poshtaukr@ukr.net>
2017-04-14 00:21:51 +0300

    second commit

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>Site</h1>
-	<p> first step</p>
-	<p> new changes in index.php</p>
+	<p>lorem</p>
+	<p>lalala</p>
 </body>
 </html>
