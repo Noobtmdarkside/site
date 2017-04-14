cbc6fe39ac60343296459261d74288422a1fc271
Andriy Dreychan <poshtaukr@ukr.net>
2017-04-14 12:39:28 +0300

    commit number 4

--- index.php
+++ index.php
@@ -13,5 +13,6 @@
 	<p> new changes in index.php</p>
 	<p> second changes in index.php for commit 2</p>
 >>>>>>> 51b7e578f53a46a8b63f7121d4c2e5e9ef13e11f
+	<p> 4 changes in index.php for commit 4</p>
 </body>
 </html>
