ff821afe4ff37d1d2f8fc7b50a43e0ab3f735276
Andriy Dreychan <poshtaukr@ukr.net>
2017-04-20 21:51:08 +0300

    first commit

--- index.php
+++ index.php
@@ -5,16 +5,7 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<<<<<<< HEAD
-<p>lore m</p>
-=======
-	<h1>Site</h1>
-	<p> first step</p>
-	<p> new changes in index.php</p>
-	<p> second changes in index.php for commit 2</p>
->>>>>>> 51b7e578f53a46a8b63f7121d4c2e5e9ef13e11f
-	<p> 4 changes in index.php for commit 4</p>
-	<p> changes on remote repository</p>
-	<h2>changes on local rep.</h2>
+<p>Перший комміт 20-04</p>
+
 </body>
 </html>
